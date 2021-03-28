28acf2c9e9c1cde98947797545e6261bf90af959
Naimah Muhammed-Yakubu <naimah.muhammed-yakubu@uwe.ac.uk>
2021-03-28 14:24:28 +0100

    Delete FendCapstoneProject7  directory

--- FendCapstoneProject7 /src/tests/city.test.js
+++
@@ -1,7 +0,0 @@
-import { cityLocation } from '../client/js/city';
-
-describe('Testing the city location', () => {
-    test('test city location', () => {
-        expect(cityLocation).toBeDefined();
-    });
-})
--- FendCapstoneProject7 /src/tests/countdown.test.js
+++
@@ -1,7 +0,0 @@
-import { countdown } from '../client/js/countdown';
-
-describe('countdown functionality', () => {
-    test('test countdown', () => {
-        expect(countdown).toBeDefined();
-    });
-})
